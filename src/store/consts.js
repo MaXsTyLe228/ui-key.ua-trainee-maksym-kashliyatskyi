89246ce82fbb7be195f3e6eb2a329ece97247f35
MaXsTyLe228 <kashlyatsky@gmail.com>
2021-12-25 18:08:46 +0200

    localhost

--- src/store/consts.js
+++ src/store/consts.js
@@ -1,4 +1,4 @@
-const PATH = 'https://v2hl01gc6g.execute-api.us-east-2.amazonaws.com/dev'
+const PATH = 'http://localhost:3000/dev'//'https://v2hl01gc6g.execute-api.us-east-2.amazonaws.com/dev'
 //var proxy_url = 'https://cors-anywhere.herokuapp.com/';
 //'http://localhost:3000/dev'
 
